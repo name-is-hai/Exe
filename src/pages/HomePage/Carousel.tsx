dea89e20a3aaabaf25cd8ab83f36d8673077844b
name-is-hai <haiqd065@gmail.com>
2024-02-28 15:24:42 +0700

    update

--- src/pages/HomePage/Carousel.tsx
+++ src/pages/HomePage/Carousel.tsx
@@ -25,45 +25,41 @@ const CarouselRooms = ({ silder, real_width, height, sizeList, warnList, priceLi
     const { isMobile } = useScreenDetector();
 
     return (
-        <div className="relative flex flex-col items-center justify-between">
-            <Carousel plugins={[plugin.current]}
-                className="rounded-2xl "
-                onMouseEnter={plugin.current.stop}
-                onMouseLeave={plugin.current.reset}>
-                <div className={`lg:w-[1350px] md:w-[${real_width}px]`} >
-                    {!isMobile && <CarouselPrevious />}
-                    <CarouselContent>
-                        {silder.map(({ src, alt }: any, index: any) => (
-                            <CarouselItem key={index}>
-                                <AsyncImage
-                                    className='rounded-2xl'
-                                    src={src}
-                                    alt={alt}
-                                    Transition={Blur}
-                                    style={{ width: '100%', height: height, objectFit: 'cover' }}
-                                />
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                    {!isMobile && <CarouselNext />}
-                </div>
-                <div className='hidden lg:-translate-x-1/2 lg:absolute md:block lg:top-3/4 lg:left-1/2'>
-                    <CardSearch findRoom={findRoom} priceList={priceList} sizeList={sizeList} warnList={warnList} className='rounded-3xl' />
-                </div>
-                <div className='absolute block -translate-x-1/2 -translate-y-1/2 left-1/2 md:hidden'>
-                    <Drawer>
-                        <DrawerTrigger asChild>
-                            <Button className='rounded-full w-72' variant="outline">
-                                Tìm Kiếm
-                            </Button>
-                        </DrawerTrigger>
-                        <DrawerContent>
-                            <CardSearch findRoom={findRoom} priceList={priceList} sizeList={sizeList} warnList={warnList} className='border-none' />
-                        </DrawerContent>
-                    </Drawer>
-                </div>
-            </Carousel >
-        </div >
+        <Carousel plugins={[plugin.current]}
+            className="rounded-2xl "
+            onMouseEnter={plugin.current.stop}
+            onMouseLeave={plugin.current.reset}>
+            {!isMobile && <CarouselPrevious />}
+            <CarouselContent className={`w-[${real_width}px]`}>
+                {silder.map(({ src, alt }: any, index: any) => (
+                    <CarouselItem key={index}>
+                        <AsyncImage
+                            className='rounded-2xl'
+                            src={src}
+                            alt={alt}
+                            Transition={Blur}
+                            style={{ width: '100%', height: height, objectFit: 'cover' }}
+                        />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            {!isMobile && <CarouselNext />}
+            <div className='hidden lg:-translate-x-1/2 lg:absolute md:block lg:top-3/4 lg:left-1/2'>
+                <CardSearch findRoom={findRoom} priceList={priceList} sizeList={sizeList} warnList={warnList} className='rounded-3xl' />
+            </div>
+            <div className='absolute block -translate-x-1/2 -translate-y-1/2 left-1/2 md:hidden'>
+                <Drawer>
+                    <DrawerTrigger asChild>
+                        <Button className='rounded-full w-72' variant="outline">
+                            Tìm Kiếm
+                        </Button>
+                    </DrawerTrigger>
+                    <DrawerContent>
+                        <CardSearch findRoom={findRoom} priceList={priceList} sizeList={sizeList} warnList={warnList} className='border-none' />
+                    </DrawerContent>
+                </Drawer>
+            </div>
+        </Carousel >
     );
 }
 
