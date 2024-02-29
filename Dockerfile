dee67406ca91ef6a5847f2b5492be8497daa740b
name-is-hai <haiqd065@gmail.com>
2024-02-29 10:47:43 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM node:21-alpine3.18 as BUILD_IMAGE
+FROM node:21-alpine3.18 AS BUILD_IMAGE
 WORKDIR /app/exe-app
 COPY package.json .
 RUN npm install
 COPY . .
 RUN npm run build
 
-FROM node:21-alpine3.18 as PROD_IMAGE
+FROM node:21-alpine3.18 AS PROD_IMAGE
 WORKDIR /app/exe-app
 COPY --from=BUILD_IMAGE /app/exe-app/dist/ /app/exe-app/dist/
 EXPOSE 8888
--- src/pages/components/top-room.tsx
+++ src/pages/components/top-room.tsx
@@ -22,7 +22,7 @@ const TopRoom = ({ rooms, isLoading }: TopRoomProps) => {
                     <Show.When isTrue={!isLoading}>
                         {rooms.map((room: any, index: any) => (
                             <CarouselItem key={index} className="md:basis-1/5 basis-3/4">
-                                <NavLink onClick={() => { window.location.href = `/room-detail?id=${room.alt}` }} to={''}>
+                                <NavLink to={`/room-detail?id=${room.alt}`}>
                                     <RoomElement room={room} aspectRatio="portrait" />
                                 </NavLink>
                             </CarouselItem>
